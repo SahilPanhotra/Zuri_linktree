c64f566c7f5036589b208843f0c5e0d34135ac5c
SahilPanhotra <sahilpanhotra@gmail.com>
2022-11-03 20:32:42 +0530

    style:basic styling done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Contact.css"
 
 function Contact() {
   return (
@@ -13,38 +14,39 @@ function Contact() {
             <div className="form__wrapper">
               <form >
                 <div className="form__fields">
-                  <div className="name__row">
+                  <div className="name__row child">
                     <div className="first__name">
-                      <div className="input__withLablel">
+                      <div className="input__withLabel">
                         <label>First name</label>
-                        <input id="first__name" type="text" placeholder="Enter your first name" />
+                        <input className="input" id="first__name" type="text" placeholder="Enter your first name" />
                       </div>
                       </div>
                       <div className="last__name">
-                      <div className="input__withLablel">
+                      <div className="input__withLabel">
                         <label>Last name</label>
-                        <input id="last__name" type="text" placeholder="Enter your last name" />
+                        <input className="input" id="last__name" type="text" placeholder="Enter your last name" />
                       </div>
                       </div> 
                   </div>
-                </div>
-                <div className="email__container">
+                
+                <div className="email__container child">
                   <div className="input__withLabel">
                     <label >Email</label>
-                    <input type="email" id="email" placeholder="yourname@email.com" />
+                    <input className="input" type="email" id="email" placeholder="yourname@email.com" />
                   </div>
                 </div>
-                <div className="email__container">
+                <div className="message__container child">
                   <div className="input__withLabel">
                     <label >Message</label>
-                    <textarea type="text" id="message" placeholder="Send me a message and I'll reply you as soon as possible..." />
+                    <textarea className="input" type="text" id="message" placeholder="Send me a message and I'll reply you as soon as possible..." />
                   </div>
                 </div>
-                <div className="checkbox__container">
-                  <input type="checkbox" />
+                <div className="checkbox__container child">
+                  <input className="input__checkbox" type="checkbox" />
                   <span className="policyAgree">You agree to providing your data to Sahil Panhotra who may contact you.</span>
                 </div>
-                <button id="btn_submit">Send Message</button>
+                </div>
+                <button className="btn" id="btn__submit">Send Message</button>
               </form>
             </div>
           </div>
